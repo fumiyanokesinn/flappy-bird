4ae59d70f284ae07b6a263e074ecf6e2bc76e11f
shindori fumiya <shindori238@gmail.com>
2023-05-31 00:07:29 +0900

    当たり判定を実装

--- bird.gd
+++ bird.gd
@@ -7,16 +7,6 @@ var palseDirection = Vector2(0, -1).normalized()
 # 衝撃
 @onready var palse = palseDirection * palsePower
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print(palse)
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _integrate_forces(state:PhysicsDirectBodyState2D):
 	# スペースキー押下時に実行
 	if Input.is_action_pressed("fling"):
