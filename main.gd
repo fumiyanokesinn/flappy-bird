02b3f1377efec9442d01ea38fd2843bd7486913c
shindori fumiya <shindori238@gmail.com>
2023-06-15 22:25:26 +0900

    create Timer

--- main.gd
+++ main.gd
@@ -1,13 +1,25 @@
 extends Node
 
-const BLOCK_BASE_X = 1280
+const BLOCK_BASE_X = 1500
 var dokan_cnt = 0
 
 var score = 0
+var startTimer = 3
 
 @onready var Count = $HUD/VBoxContainer/Count
+@onready var StartTimer = $HUD/StartCount/StartTimer
+@onready var StartCount = $HUD/StartCount
+
 var Block = preload("res://block.tscn")
 
+func start_count():
+	StartCount.show()
+	StartTimer.start()
+	get_tree().paused = true
+	
+
+func _ready():
+	start_count()
 
 # 土管が画面外に行った時に土管を再出現する
 func _on_block_tree_exiting():
@@ -24,7 +36,7 @@ func _on_block_tree_exiting():
 	
 	
 	# 土管の速度を設定
-	block.velocity.x = -200 * dokan_cnt + block.velocity.x
+#	block.velocity.x = -200 * dokan_cnt + block.velocity.x
 	
 	# 土管を再出現
 	add_child.call_deferred(block)
@@ -32,3 +44,17 @@ func _on_block_tree_exiting():
 func _on_point_area_body_exited(body):
 	score += 1
 	Count.text = str(score)
+
+
+func _on_start_timer_timeout():
+	
+	if startTimer == 0:
+		# 3秒経過後
+		startTimer = 3
+		StartCount.hide()
+		StartTimer.stop()
+		get_tree().paused = false
+	else:
+		# 3秒未経過
+		startTimer -= 1
+	StartCount.text = str(startTimer)
