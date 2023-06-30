c7f290ca6f477f17fb524d2d4611b973dc009cc2
shindori fumiya <shindori238@gmail.com>
2023-06-30 21:31:31 +0900

    add move Button

--- RichTextLabel.gd
+++ RichTextLabel.gd
@@ -1,18 +1,8 @@
 extends RichTextLabel
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _on_mouse_entered():
-	text = "	[center][shake rate=5.0 level=40 connected=1]START"
+	text = "[center][shake rate=5.0 level=40 connected=1]%s" % [get_parsed_text()]
 
 
 func _on_mouse_exited():
-	text = "	[center]START"
+	text = "[center]%s" % [get_parsed_text()]
