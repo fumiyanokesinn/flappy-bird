f927c38c694415d5a651d3b4168aa82ccdc47481
shindori fumiya <shindori238@gmail.com>
2023-07-17 13:14:38 +0900

    add script PlayerName

--- global.gd
+++ global.gd
@@ -2,4 +2,6 @@ extends Node
 
 var isGaming = true
 
+var playerName = ''
+
 const DARK_BLUE = Color(0, 0, 0.545098, 1)
--- start.gd
+++ start.gd
@@ -3,7 +3,11 @@ extends Control
 var Main = load("res://main.tscn")
 var Ranking = load("res://ranking.tscn")
 
+func _ready():
+	$PlayerName.text = Global.playerName
+
 func _on_start_button_pressed():
+	Global.playerName = $PlayerName.text
 	$ButtonClick.play()
 	Global.isGaming = true
 	SceneTransction.change_scene(Main,Global.DARK_BLUE )
