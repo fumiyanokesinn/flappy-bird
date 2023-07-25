85db0995136adc57a6d5e0748e60ddc22458cbc1
shindori fumiya <shindori238@gmail.com>
2023-07-25 23:23:28 +0900

    fix ranking

--- ranking.gd
+++ ranking.gd
@@ -28,15 +28,15 @@ func showUsers(users : Array):
 # RichTextLabelのTextに値を格納する
 func setLabeltext(label : RichTextLabel,user : Dictionary,index : int):
 	if index == 0:
-		label.text = "[center]1st：[wave amp=50 freq=5]%s : %s" % [user.name,user.score]
+		label.text = "[center]1st:[wave amp=50 freq=5]%s : %s" % [user.name,user.score]
 	elif index == 1:
-		label.text = "[center]2nd：[wave amp=50 freq=5]%s : %s" % [user.name,user.score]
+		label.text = "[center]2nd:[wave amp=50 freq=5]%s : %s" % [user.name,user.score]
 	elif index == 2:
-		label.text = "[center]3rd：[wave amp=50 freq=5]%s : %s" % [user.name,user.score]
+		label.text = "[center]3rd:[wave amp=50 freq=5]%s : %s" % [user.name,user.score]
 	elif index == 3 :
-		label.text = "[center]4th：%s : %s" % [user.name,user.score]
+		label.text = "[center]4th:%s : %s" % [user.name,user.score]
 	elif index == 4 :
-		label.text = "[center]5th：%s : %s" % [user.name,user.score]
+		label.text = "[center]5th:%s : %s" % [user.name,user.score]
 	pass
 
 # User検索APIのレスポンスを作成
